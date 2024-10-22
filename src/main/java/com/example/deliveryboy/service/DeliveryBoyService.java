5d7f38e7632fe09b49837c4035dfb1d7565dfa9e
Anii5123 <anipatil5122003@gmail.com>
2024-10-22 23:19:44 +0530

    first commit

---
+++ src/main/java/com/example/deliveryboy/service/DeliveryBoyService.java
@@ -0,0 +1,32 @@
+package com.example.deliveryboy.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.deliveryboy.entity.DeliveryBoy;
+import com.example.deliveryboy.repository.DeliveryBoyRepository;
+
+@Service
+public class DeliveryBoyService {
+
+    @Autowired
+    private DeliveryBoyRepository repository;
+
+    public List<DeliveryBoy> getAllDeliveryBoys() {
+        return repository.findAll();
+    }
+
+    public DeliveryBoy getDeliveryBoyById(Long empId) {
+        return repository.findById(empId).orElse(null);
+    }
+
+    public DeliveryBoy saveDeliveryBoy(DeliveryBoy deliveryBoy) {
+        return repository.save(deliveryBoy);
+    }
+
+    public void deleteDeliveryBoy(Long empId) {
+        repository.deleteById(empId);
+    }
+}
